276500527539186babf7998ac1b200f1692d9df7
Max Kaplan <kaplanmaxe3@gmail.com>
2018-02-11 09:24:39 -0500

    testing typedoc

---
+++ typedoc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  exit 0
+fi
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+yarn add typedoc
+git clone -b gh-pages https://${GH_TOKEN}@github.com/kaplanmaxe/kraken-api.git typedoc
+typedoc --out docs/ src/**/*.ts
+git add .
+git commit -m "adding documentation"
+git push -u origin gh-pages
